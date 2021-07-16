2476288caaa8c937764ad70274d99968c943f1ce
Jeppe Frandsen <jeppe@algrens.dk>
2021-07-16 21:20:08 +0200

    Simplified build script

--- build.sh
+++ build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 set -e
 
-CMAKE_ARGS="-DBUILD_EXTERNAL=1 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc.cmake"
-
-if [ "$1" = "example" ]; then
-  CMAKE_ARGS="$CMAKE_ARGS -DBUILD_EXAMPLE=1 -DBUILD_TESTING=0"
-fi
-
 mkdir -p .build-external; pushd .build-external
 cmake ../external
 make -j "$(nproc)"
 popd
 
 mkdir -p .build-x86; pushd .build-x86
-cmake "$CMAKE_ARGS" ..
+cmake -DBUILD_EXTERNAL=1 -DBUILD_EXAMPLE=1 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc.cmake ..
 make -j "$(nproc)"
 
 if [ "$1" = "coverage" ]; then
@@ -21,7 +15,7 @@ if [ "$1" = "coverage" ]; then
   ctest --verbose
   lcov -q -c -d . -o test.info 2>/dev/null
   lcov -q -a base.info -a test.info > total.info
-  lcov -q -r total.info "*usr/include/*" "*CMakeFiles*" "*/test/*" "*Catch2*" "*turtle*" -o coverage.info
+  lcov -q -r total.info "*usr/include/*" "*CMakeFiles*" "*Catch2*" "*turtle*" "*/test/*" "*src/*" -o coverage.info
   genhtml -o coverage coverage.info
   echo "Coverage report can be found in $(pwd)/coverage"
 fi
