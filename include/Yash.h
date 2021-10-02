51bc97c0c6eed087bfd1316a1909e0e179f24336
Jeppe Frandsen <jeppe@algrens.dk>
2021-10-02 17:12:34 +0200

    Add command to description

--- include/Yash.h
+++ include/Yash.h
@@ -410,8 +410,11 @@ private:
             if (descriptions.empty()) {
                 for (const auto& [command, function] : m_functions) {
                     auto position = command.find_first_of(s_commandDelimiter);
-                    if (position != std::string::npos)
-                        descriptions.emplace(command.substr(0, position), "Commands");
+                    if (position != std::string::npos) {
+                        std::string cmd = command.substr(0, position);
+                        cmd[0] = toupper(cmd[0]);
+                        descriptions.emplace(command.substr(0, position), cmd + " commands");
+                    }
                     else
                         descriptions.emplace(command, function.m_description);
                 }
